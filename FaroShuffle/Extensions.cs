a6ec2bd57572fa524be5da46c0f1d034e99fde1e
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2022-03-02 11:17:54 -0700

    Playing around with LINQ

---
+++ FaroShuffle/Extensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FaroShuffle
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+
+        public static bool SequenceEquals<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                if (!firstIter.Current.Equals(secondIter.Current))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static IEnumerable<T> LogQuery<T>(this IEnumerable<T> sequence, string tag)
+        {
+            using (var writer = File.AppendText("debug.log"))
+            {
+                writer.WriteLine($"Execution Query {tag}");
+            }
+
+            return sequence;
+        }
+    }
+}
--- FaroShuffle/Program.cs
+++ FaroShuffle/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FaroShuffle
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            // Query Syntax
+            var startingDeck = (from s in Suits().LogQuery("Suit Generation")
+                               from r in Ranks().LogQuery("Rank Generation")
+                               select new { Suit = s, Rank = r })
+                               .LogQuery("Starting Deck")
+                               .ToArray();
+
+            // Method Syntax
+            //var startingDeck = Suits().SelectMany(suit => Ranks().Select(rank => new { Suit = suit, Rank = rank }));
+
+            var top = startingDeck.Take(26);
+            var bottom = startingDeck.Skip(26);
+            //var shuffle = top.InterleaveSequenceWith(bottom);
+
+            var times = 0;
+
+            var shuffle = startingDeck;
+            do
+            {
+                shuffle = shuffle.Skip(26).LogQuery("Bottom Half")
+                    .InterleaveSequenceWith(shuffle.Take(26)).LogQuery("Top Half")
+                    .LogQuery("Shuffle")
+                    .ToArray();
+
+                foreach (var card in shuffle)
+                {
+                    Console.WriteLine(card);
+                }
+                Console.WriteLine();
+                times++;
+            } while (!startingDeck.SequenceEquals(shuffle));
+
+            Console.WriteLine(times);
+        }
+
+        static IEnumerable<string> Suits()
+        {
+            yield return "clubs";
+            yield return "diamons";
+            yield return "hearts";
+            yield return "spades";
+        }
+
+        static IEnumerable<string> Ranks()
+        {
+            yield return "two";
+            yield return "three";
+            yield return "four";
+            yield return "five";
+            yield return "six";
+            yield return "seven";
+            yield return "eight";
+            yield return "nine";
+            yield return "ten";
+            yield return "jack";
+            yield return "queen";
+            yield return "king";
+            yield return "ace";
+        }
+    }
+}
